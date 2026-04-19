09a40ed747423116388d1ed91ff0c96c22a96f83
sushant347 <sushant98677@gmail.com>
2026-04-19 19:28:46 +0545

    chore(frontend): bootstrap React root with boundary wrapper

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import AppErrorBoundary from './components/AppErrorBoundary.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AppErrorBoundary>
+    <App />
+  </AppErrorBoundary>,
+)
